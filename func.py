30c19fa511e0c5169b0296a12cff888beae35843
Seonghyun Park <tompark122@gmail.com>
2023-03-15 14:41:50 +0900

    sorted out

--- func.py
+++ func.py
@@ -37,6 +37,7 @@ fourAc = mult(4,(mult(a,c)))
 expB = exp(b,2)
 BfAC = sub(expB,fourAc)
 twoA = mult(2,a)
+
 x1 = div(add(neg(b),sqrt(BfAC)),twoA) # TODO: write a code to compute the first root of the quadratic equation
 x2 = div(sub(neg(b),sqrt(BfAC)),twoA) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
